414b52506c94dbaee9cdffe739dbaad21699785a
sunlight97 <piao_ing@outlook.com>
2022-06-02 21:58:29 +0800

    c++: 4a watermelon

---
+++ round_4/4A_Watermelon.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int weight {0};
+	cin >> weight;
+	if(weight % 2 == 0 && weight > 3)
+		cout << "YES" << "\n";
+	else
+		cout << "NO" << "\n";
+	return 0;
+}
+
